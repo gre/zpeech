47d6d3d281a9ebdf55704fca247972fce3f5ffd1
Nizar Sehli <nse@zenexity.com>
2013-10-31 14:36:42 +0100

    Simple moving average function

--- src/extractFormants.js
+++ src/extractFormants.js
@@ -4,9 +4,11 @@ function extractFormants (frequenciesBuffer, indexToFrequency, valueToPercent) {
   var size = frequenciesBuffer.length;
   var f1, f2;
   var maxf1 = -Infinity, maxf2 = -Infinity;
+  var sma = simple_moving_averager(4);
+
   _.each(frequenciesBuffer, function (value, i) {
     var freq = indexToFrequency(i);
-    var db = valueToPercent(value);
+    var db = valueToPercent(sma(value));
     if (freq < 1500) {
       if (db > maxf1) {
         maxf1 = db;
@@ -27,6 +29,22 @@ function extractFormants (frequenciesBuffer, indexToFrequency, valueToPercent) {
     db: maxf2,
     freq: f2
   }];
+}
+
+function simple_moving_averager(period) {
+    var nums = [];
+    return function(num) {
+        nums.push(num);
+        if (nums.length > period)
+            nums.splice(0,1);  // remove the first element of the array
+        var sum = 0;
+        for (var i in nums)
+            sum += nums[i];
+        var n = period;
+        if (nums.length < period)
+            n = nums.length;
+        return(sum/n);
+    }
 }
 
 module.exports = extractFormants;
