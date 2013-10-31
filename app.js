26b111f43660fcdae4f94f07804d8d8baadd2dd5
Gaëtan Renaudeau <gre@zenexity.com>
2013-10-31 12:13:40 +0100

    progress

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 
 var AudioContext = require("audiocontext");
+var _ = require("lodash");
 var formantsToVowel = require("./src/formantsToVowel");
 var extractFormants = require("./src/extractFormants");
 var getMicrophone = require("./src/getMicrophone");
+var Spectrum = require("./src/spectrum");
 
 if (!AudioContext) {
   alert("no AudioContext support available in your browser.");
@@ -29,17 +31,33 @@ function valueToPercent (value) {
   return value / 256;;
 }
 
+var formants = [];
 var $vowel = document.getElementById("vowel");
 maybeMicrophone.then(function(mic){
   setInterval(function(){
     analyzer.getByteFrequencyData(array);
-    var formants = extractFormants(array, indexToFrequency, valueToPercent);
+    formants = extractFormants(array, indexToFrequency, valueToPercent);
     var vowel = formantsToVowel.apply(this, formants);
     if (vowel) {
       $vowel.innerHTML = vowel;
+      $vowel.className = "";
     }
     else {
-      $vowel.innerHTML = "";
+      $vowel.className = "hidden";
     }
   }, 100);
 });
+
+(function(canvas) {
+  var canvasCtx = canvas.getContext("2d");
+  var spectrum = new Spectrum(canvasCtx, array);
+  function loop () {
+    requestAnimationFrame(function () {
+      loop();
+      spectrum.render(_.map(formants, function (f) {
+        return Math.floor(f.freq * analyzer.fftSize / ctx.sampleRate);
+      }));
+    });
+  }
+  maybeMicrophone.then(loop);
+}(document.getElementById("frequencies")));
--- src/formantsToVowel.js
+++ src/formantsToVowel.js
@@ -1,7 +1,6 @@
 function formantsToVowel (f1, f2, f3) {
   if (f1.freq < 200) return null;
-  console.log(arguments);
-  if (f2.db > .3) {
+  if (f2.db > 0.5*f1.db) {
     if (f1.freq > 200 && f1.freq < 400 && 
         f2.freq > 3000 && f2.freq < 3500) {
           return "i";
