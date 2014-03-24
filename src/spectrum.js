02814a85d230be443f165b69f03e5f3fba8da6a7
Gaëtan Renaudeau <gre@zenexity.com>
2014-03-24 14:46:07 +0100

    LICENCE

--- src/spectrum.js
+++ src/spectrum.js
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Zpeech.
+ *
+ * Copyright 2014 Zengularity
+ *
+ * Zpeech is free software: you can redistribute it and/or modify
+ * it under the terms of the AFFERO GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * Zpeech is distributed "AS-IS" AND WITHOUT ANY WARRANTY OF ANY KIND,
+ * INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
+ * NON-INFRINGEMENT, OR FITNESS FOR A PARTICULAR PURPOSE. See
+ * the AFFERO GNU General Public License for the complete license terms.
+ *
+ * You should have received a copy of the AFFERO GNU General Public License
+ * along with Zpeech.  If not, see <http://www.gnu.org/licenses/agpl-3.0.html>
+ */
 
 function Spectrum (ctx, array) {
   this.ctx = ctx;
